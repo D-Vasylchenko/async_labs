4de1d1e2ab735741f8e88706fd14b3af97d81ccc
D-Vasylchenko <145137182+D-Vasylchenko@users.noreply.github.com>
2024-12-23 15:49:53 +0200

    added test cases to promise-all.js, created lab2.js

--- task2/promise-all.js
+++ task2/promise-all.js
@@ -7,3 +7,78 @@ const filterWithPromises = (items, asyncPredicate) => {
     ).then((results) => results.filter((item) => item !== null));
 };
 
+//test cases
+const numericArray = [10, 15, 20, 25, 30, 35];
+const charArray = ['x', 'y', 'z', 'h', 'e', 'o'];
+
+// test case 1:filter eeven numbers
+filterWithPromises(
+    numericArray,
+    (number) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(number % 2 === 0);
+            }, 500);
+        });
+    }
+)
+    .then((result) => {
+        console.log('Even numbers:', result);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+
+//test case 2:filter characters
+filterWithPromises(
+    charArray,
+    (char) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve('hello'.includes(char));
+            }, 500);
+        });
+    }
+)
+    .then((result) => {
+        console.log('Characters in "hello":', result);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+
+//test case 3:handle rejection in the predicate
+filterWithPromises(
+    charArray,
+    (char) => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                reject(new Error('Test rejection error'));
+            }, 500);
+        });
+    }
+)
+    .then((result) => {
+        console.log('Filtered characters:', result);
+    })
+    .catch((error) => {
+        console.error('Caught error:', error.message);
+    });
+
+//test case 4: filter numbers/20
+filterWithPromises(
+    numericArray,
+    (number) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(number > 20);
+            }, 500);
+        });
+    }
+)
+    .then((result) => {
+        console.log('Numbers greater than 20:', result);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
